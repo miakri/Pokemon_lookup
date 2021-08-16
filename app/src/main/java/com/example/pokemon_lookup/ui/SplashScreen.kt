9fe72f1905ce7f6bc3f4ff73e28d46ea1f40d2c8
miakri <43073917+miakri@users.noreply.github.com>
2021-08-16 02:24:27 +0200

    splash screen with working animation

---
+++ app/src/main/java/com/example/pokemon_lookup/ui/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.example.pokemon_lookup.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pokemon_lookup.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+        },6500)
+    }
+}
